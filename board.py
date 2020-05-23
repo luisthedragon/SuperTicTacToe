e083e7dd03792062b51d7d85b57168b2cd3affd0
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-05-22 22:23:15 -0500

    Renamed objects to make sense

--- board.py
+++ board.py
@@ -1,3 +1,6 @@
+from miniboard import Miniboard
+
+
 class Board:
     WINNING_STATES = [[[1, 1, 1],
                        [0, 0, 0],
@@ -25,25 +28,26 @@ class Board:
                        [1, 0, 0]]]
 
     def __init__(self):
-        self.state = [[[[0 for i in range(3)] for j in range(3)] for k in range(3)] for l in range(3)]
-        self.section_owners = [[0 for i in range(3)] for j in range(3)]
-        self.last_marked_cell = [-1, -1, -1, -1]
+        self.state = [[Miniboard() for i in range(3)] for j in range(3)]
+        # self.section_owners = [[0 for i in range(3)] for j in range(3)]
+        self.last_marked_section = [-1, -1]
         self.game_over = False
 
     # Only for debug purpose
     def print_state(self, state):
         for i in range(3):
             for j in range(3):
-                print(state[i][0][j], state[i][1][j], state[i][2][j])
+                print(state[i][0].state[j], state[i][1].state[j], state[i][2].state[j])
             print(' ')
 
     def mark(self, state, player_id, section_row, section_col, element_row, element_col):
         if self.check_valid_mark(state, section_row, section_col, element_row, element_col):
-            state[section_row][section_col][element_row][element_col] = player_id
-            self.last_marked_cell[0] = section_row
-            self.last_marked_cell[1] = section_col
-            self.last_marked_cell[2] = element_row
-            self.last_marked_cell[3] = element_col
+            state[section_row][section_col].mark(state[section_row][section_col].state, player_id, element_row,
+                                                 element_col)
+            self.last_marked_section[0] = section_row
+            self.last_marked_section[1] = section_col
+            self.state[section_row][section_col].last_marked_cell[0] = element_row
+            self.state[section_row][section_col].last_marked_cell[1] = element_col
             self.check_section_owned(state, section_row, section_col)
             return True
         return False
@@ -52,10 +56,10 @@ class Board:
         if self.check_out_of_bounds(section_row, section_col, element_row, element_col):
             print('out of bounds')
             return False
-        elif state[section_row][section_col][element_row][element_col] != 0:
+        elif state[section_row][section_col].state[element_row][element_col] != 0:
             print('already marked cell')
             return False
-        elif self.section_owners[section_row][section_col] != 0:
+        elif state[section_row][section_col].owner != 0:
             print('section owned')
             return False
         elif not self.check_unlocked_section(section_row, section_col):
@@ -69,34 +73,39 @@ class Board:
         return True
 
     def check_unlocked_section(self, section_row, section_col):
-        if self.last_marked_cell[0] == -1:
+        if self.last_marked_section[0] == -1:
             # print('First turn')
             return True
-        if self.section_owners[self.last_marked_cell[2]][self.last_marked_cell[3]] != 0:
+        if self.get_section_to_unlock(self.state).owner != 0:
             # print('Section to unlock is owned: all sections unlocked')
             return True
-        if section_row == self.last_marked_cell[2] and section_col == self.last_marked_cell[3]:
+        if [section_row, section_col] == self.get_last_marked_cell_of_last_marked_section(self.state):
             # print('Last marked cell unlock this section')
             return True
         return False
 
+    def get_section_to_unlock(self, state):
+        return state[self.get_last_marked_cell_of_last_marked_section(state)[0]][
+            self.get_last_marked_cell_of_last_marked_section(state)[1]]
+
+    def get_last_marked_cell_of_last_marked_section(self, state):
+        return state[self.last_marked_section[0]][self.last_marked_section[1]].last_marked_cell
+
     def check_section_owned(self, state, section_row, section_col):
-        for winning_state in self.WINNING_STATES:
-            if self.check_grid_matches_winning_state(state[section_row][section_col], winning_state, 1):
-                self.section_owners[section_row][section_col] = 1
-                return True
-            if self.check_grid_matches_winning_state(state[section_row][section_col], winning_state, 2):
-                self.section_owners[section_row][section_col] = 2
-                return True
+        if state[section_row][section_col].owner != 0:
+            return True
         return False
 
     def check_game_over(self, state):
         for winning_state in self.WINNING_STATES:
-            if self.check_grid_matches_winning_state(self.section_owners, winning_state, 1):
+            if self.check_grid_matches_winning_state(self.get_section_owners(state), winning_state, 1):
                 return True, 1
-            if self.check_grid_matches_winning_state(self.section_owners, winning_state, 2):
+            if self.check_grid_matches_winning_state(self.get_section_owners(state), winning_state, 2):
                 return True, 2
         return False, 0
+
+    def get_section_owners(self, state):
+        return [[state[i][j].owner for j in range(3)] for i in range(3)]
 
     def check_grid_matches_winning_state(self, grid, winning_state, player_id):
         for i in range(3):
--- tests/test1.py
+++ tests/test1.py
@@ -1,57 +1,24 @@
-"""
-*** *** ***
-*** *** ***
-*** *** ***
-
-*** *** ***
-*** *** ***
-*** *** ***
-
-*** *** ***
-*** *** ***
-*** *** ***
-"""
-
-import pygame
 from board import Board
+import pygame
+game_board = Board()
 
-# Main without using pygame (last update: 09/05/2020) (Probably won't work)
-# def main():
-#     state = [[[[0 for i in range(3)] for j in range(3)] for k in range(3)] for l in range(3)]
-#     state[0][0][0][0] = state[0][0][1][1] = state[0][0][2][2] = state[1][1][1][2] = 1
-    # print_state(state)
-    # print(check_section_owned(state, 0, 0))
-    # print(mark(state, 1, 0, 0, 0, 0))
-    # print(mark(state, 2, 0, 0, 1, 1))
-    # print(mark(state, 2, 1, 2, 0, 0))
-    # print(mark(state, 2, 2, 2, 0, 0))
-    # print(mark(state, 1, 0, 0, 2, 2))
-    # print(mark(state, 2, 1, 1, 1, 2))
-    # print(mark(state, 1, 2, 2, 0, 2))
-    # print(mark(state, 2, 2, 2, 1, 2))
-    # print(mark(state, 1, 1, 1, 1, 2))
-    # print(mark(state, 1, 1, 2, 1, 2))
-    # print_state(state)
-    # Testing check_game_over function
-    # section_owners[0][0] = section_owners[0][1] = section_owners[0][2] = 2
-
-    # main loop
-    # player_id = 1
-    # while True:
-    #     mark_request = list(map(int, input().split()))
-    #     while not mark(state, player_id, mark_request[0], mark_request[1], mark_request[2], mark_request[3]):
-    #         mark_request = list(map(int, input().split()))
-    #     player_id = player_id % 2 + 1
-    #     print_state(state)
-    #     print(section_owners)
-
-
-# main()
-
-
-# state = [[[[0 for i in range(3)] for j in range(3)] for k in range(3)] for l in range(3)]
-# state[0][0][0][0] = state[0][0][1][1] = state[0][0][2][2] = state[1][1][1][2] = 1
-
+# bigboard.print_state(bigboard.state)
+# bigboard.mark(bigboard.state, 1, 0, 0, 0, 0)
+# bigboard.mark(bigboard.state, 2, 0, 0, 1, 1)
+# bigboard.mark(bigboard.state, 1, 1, 1, 0, 0)
+# bigboard.mark(bigboard.state, 2, 0, 0, 2, 1)
+# bigboard.mark(bigboard.state, 1, 2, 1, 0, 0)
+# bigboard.mark(bigboard.state, 2, 0, 0, 0, 1)
+# print('-------------------------------------')
+# bigboard.print_state(bigboard.state)
+# bigboard.mark(bigboard.state, 1, 0, 1, 0, 0)
+# bigboard.mark(bigboard.state, 2, 0, 2, 1, 0)
+# print('-------------------------------------')
+# bigboard.print_state(bigboard.state)
+# bigboard.mark(bigboard.state, 1, 1, 1, 1, 1)
+# bigboard.mark(bigboard.state, 2, 2, 2, 1, 0)
+# print('-------------------------------------')
+# bigboard.print_state(bigboard.state)
 
 pygame.init()
 
@@ -61,7 +28,7 @@ width, height = 50, 50
 padding = (win_width - width * 9 - space_between_sections * 2) // 2
 
 win = pygame.display.set_mode((win_width, win_height))
-board = Board()
+game_board = Board()
 
 pygame.display.set_caption("SUPER TIC TAC TOE")
 
@@ -84,7 +51,7 @@ while run:
                              (event.pos[0] - padding) % (space_between_sections + 3 * width) // width
                 print(i, j, k, l)
 
-                if board.mark(board.state, current_player_id, i, j, k, l):
+                if game_board.mark(game_board.state, current_player_id, i, j, k, l):
                     current_player_id = current_player_id % 2 + 1
 
     # pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
@@ -97,9 +64,9 @@ while run:
         for j in range(3):
             for k in range(3):
                 for l in range(3):
-                    if board.state[j][i][l][k] == 0:
+                    if game_board.state[j][i].state[l][k] == 0:
                         color = (255, 0, 0)
-                    elif board.state[j][i][l][k] == 1:
+                    elif game_board.state[j][i].state[l][k] == 1:
                         color = (0, 255, 0)
                     else:
                         color = (0, 0, 255)
@@ -109,24 +76,24 @@ while run:
                     pygame.draw.rect(win, color, (padding + i * (width * 3 + space_between_sections) + k * width + 1,
                                                   padding + j * (height * 3 + space_between_sections) + l * height + 1,
                                                   width, height))
-            unlocked_sections = board.get_unlocked_sections()
+            unlocked_sections = game_board.get_unlocked_sections()
             s = pygame.Surface((150, 150), pygame.SRCALPHA)  # per-pixel alpha
-            if board.game_over:
+            if game_board.game_over:
                 if winner == 1:
                     s.fill((0, 255, 0, 220))  # notice the alpha value in the color
                 else:
                     s.fill((0, 0, 255, 220))  # notice the alpha value in the color
-            elif board.section_owners[j][i] == 1:
+            elif game_board.get_section_owners(game_board.state)[j][i] == 1:
                 s.fill((0, 255, 0, 220))  # notice the alpha value in the color
-            elif board.section_owners[j][i] == 2:
+            elif game_board.get_section_owners(game_board.state)[j][i] == 2:
                 s.fill((0, 0, 255, 220))  # notice the alpha value in the color
             elif unlocked_sections[j][i] == 1:
                 s.fill((255, 255, 255, 128))  # notice the alpha value in the color
             win.blit(s, (padding + i * (width * 3 + space_between_sections), padding + j * (height * 3 +
                                                                                             space_between_sections)))
-    if board.check_game_over(board.state)[0]:
+    if game_board.check_game_over(game_board.state)[0]:
         # print('winner is: Player ', check_game_over(state)[1])
-        board.game_over, winner = True, board.check_game_over(board.state)[1]
+        game_board.game_over, winner = True, game_board.check_game_over(game_board.state)[1]
 
     # s = pygame.Surface((500, 500), pygame.SRCALPHA)  # per-pixel alpha
     # s.fill((255, 255, 255, 128))  # notice the alpha value in the color
